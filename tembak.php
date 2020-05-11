cb4c89af4b190f599d74ed28a4180abd9dd5159d
Alif Dzikri <55616294+dziunincode69@users.noreply.github.com>
2020-05-11 21:53:48 +0700

    Update tembak.php

--- tembak.php
+++ tembak.php
@@ -12,7 +12,7 @@ for ($i=0; $i < $jumlah; $i++)
 {
 $token = $pemisah[$o];
 $poin_awal = cek_Poin($token);
-echo "\n $token \n"; echo "[+]Poin awal : $poin_awal \n";
+echo "\n $token \n"; echo "[+]Poin awal : $poin_awal \n Process.. \n";
 $isi_Quiz = isi_Quiz($token);
 echo "[+]Poin Akhir : ".cek_Poin($token)." \n";
 $o++;
