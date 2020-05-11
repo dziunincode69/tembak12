09a5910400110842ee1663daa7e453ea99de66b7
Alif Dzikri <55616294+dziunincode69@users.noreply.github.com>
2020-05-11 21:50:50 +0700

    Update tembak.php

--- tembak.php
+++ tembak.php
@@ -2,19 +2,17 @@
 //Watch
 require 'class-tembak.php';
 error_reporting(0);
-system('ls token');
 $o =0;
-echo "? File ";
+echo "? File token list ";
 $file = trim(fgets(STDIN));
 $list = file_get_contents($file);
 $pemisah = explode("\n", $list);
 $jumlah = count($pemisah);
 for ($i=0; $i < $jumlah; $i++) 
 {
-$mailpass = explode("|", $pemisah[$o]);
-$email = $mailpass[0]; $password = $mailpass[1]; $token = $mailpass[2];
+$token = $pemisah[$o];
 $poin_awal = cek_Poin($token);
-echo "\n $email | $token \n"; echo "[+]Poin awal : $poin_awal \n";
+echo "\n $token \n"; echo "[+]Poin awal : $poin_awal \n";
 $isi_Quiz = isi_Quiz($token);
 echo "[+]Poin Akhir : ".cek_Poin($token)." \n";
 $o++;
